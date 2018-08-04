4d6e8cc16ac74ed6665a904f99fd681793869a84
Rodolfo Reina Figueroa <rodolfo.reina@outlook.com>
2018-08-03 21:41:00 -0500

    Clase 14. Parte 1

--- pages/dibujo2/dibujo.js
+++ pages/dibujo2/dibujo.js
@@ -1,12 +1,32 @@
 var vObjCanvas = document.getElementById('dibujo');
 var vObjLienzo = vObjCanvas.getContext('2d');
+var vNumLineas = document.getElementById('numLineas');
+var vBtnAceptar = document.getElementById('btnAceptar');
 
-for (var i = 0; i< 300;i = i +10 )
+vBtnAceptar.addEventListener("click", dibujoPorClick);
+
+function pintarCanvas(pvNumLineas)
 {
-  dibujarLinea('blue', 0, i, i + 10, 300);
-  dibujarLinea('red', i, 0, 300, i + 10);
-  dibujarLinea('green', i, 300, 300, 300 - (i + 10));
-  dibujarLinea('yellow', 300 - i, 0, 0, i + 10);
+  limpiarCanvas();
+  for (var i = 0; i< pvNumLineas * 10;i = i + 10 )
+  {
+    dibujarLinea('blue', 0, i, i + 10, 300);
+    dibujarLinea('red', i, 0, 300, i + 10);
+    dibujarLinea('green', i, 300, 300, 300 - (i + 10));
+    dibujarLinea('yellow', 300 - i, 0, 0, i + 10);
+  }
+}
+
+function dibujoPorClick()
+{
+  if (vNumLineas.value > 0 && vNumLineas.value <= 30)
+  {
+    pintarCanvas(vNumLineas.value);
+  }
+  else
+  {
+      alert('El valor ingresado no es valido.');
+  }
 }
 
 function dibujarLinea(color, inicioX, inicioY, finX, finY)
@@ -17,4 +37,9 @@ function dibujarLinea(color, inicioX, inicioY, finX, finY)
   vObjLienzo.lineTo(finX, finY);
   vObjLienzo.stroke();
   vObjLienzo.closePath();
+}
+
+function limpiarCanvas()
+{
+    vObjLienzo.clearRect(0, 0, vObjCanvas.width, vObjCanvas.height);
 }
